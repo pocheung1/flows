7844bc62f53c680cf08ba501c3fb9c4836bcdd72
Po Cheung <po.cheung@dominodatalab.com>
2024-08-20 17:13:05 -0700

    Add dataset_workflow.py

--- dataset_workflow.py
+++ dataset_workflow.py
@@ -1,5 +1,6 @@
 from flytekit import workflow
 from flytekitplugins.domino.task import DominoJobConfig, DominoJobTask, DatasetSnapshot
+from flytekitplugins.domino.helpers import run_domino_job_task
 
 
 @workflow
@@ -8,14 +9,37 @@ def workflow() -> None:
     pyflyte run --remote dataset_workflow.py workflow
     """
 
-    DominoJobTask(
-        name="Dataset",
-        domino_job_config=DominoJobConfig(
-            Command="cat /mnt/data/snapshots/flows/2/hello.txt",
-            DatasetSanpshots=[
-                DatasetSnapshot(Id="66b15adb4c11b830fac3fe57", Name="flows", Version=1),
-                DatasetSnapshot(Id="66b15b0c4c11b830fac3fe5b", Name="flows", Version=2),
-            ],
-        ),
-        use_latest=True,
-    )()
+    run_domino_job_task(
+        flyte_task_name="Shared datasets",
+        # command="cat /mnt/data/foo/version.txt /mnt/imported/data/flows-playground/version.txt",
+        command="ls -lR /mnt/data /mnt/imported/data",
+        dataset_snapshots=[
+            # Success
+            # Dataset missing id and name
+            DatasetSnapshot(Version=1),
+            # Dataset snapshot version 0
+            DatasetSnapshot(Id="66b31d987fb95b3989a25d0f", Name="foo", Version=0),
+            # Warning: no version provided
+            # DatasetSnapshot(Id="66b31d987fb95b3989a25d0f"),
+            # Dataset name mismatch
+            DatasetSnapshot(Id="66b31d987fb95b3989a25d0f", Name="bad"),
+            # Dataset snapshot not found
+            DatasetSnapshot(Id="bad-id"),
+            # Dataset snapshot not found
+            DatasetSnapshot(Name="bad-name"),
+            # Dataset does not have any snapshots
+            DatasetSnapshot(Name="no-snapshot"),
+            # Dataset snapshot version 99 not found.
+            DatasetSnapshot(Id="66b31d987fb95b3989a25d0f", Version=99),
+            # Dataset id mismatch
+            DatasetSnapshot(Id="bad-id", Name="foo"),
+            # Multiple datasets match by name and version
+            DatasetSnapshot(Name="foo", Version=1),
+            # Multiple datasets match by name
+            DatasetSnapshot(Name="foo"),
+            # Multiple snapshots for the same dataset
+            DatasetSnapshot(Id="66a589d607325c0ea95729dd", Name="flows-playground", Version=1),
+            DatasetSnapshot(Id="66a589d607325c0ea95729dd", Name="flows-playground", Version=2),
+        ],
+        use_project_defaults_for_omitted=True
+    )
