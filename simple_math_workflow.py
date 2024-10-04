2dd818a596794ea2b0a6bcc65353df9ab2e2a854
Po Cheung <po.cheung@dominodatalab.com>
2024-10-04 13:39:49 -0700

    Add simple_math_workflow.py

---
+++ simple_math_workflow.py
@@ -0,0 +1,31 @@
+from flytekit import workflow
+from flytekitplugins.domino.task import DominoJobConfig, DominoJobTask
+
+
+@workflow
+def workflow(a: int, b: int) -> float:
+    """
+    pyflyte run --remote simple_math_workflow.py workflow --a 10 --b 6
+    """
+
+    # Create first task
+    add_task = DominoJobTask(
+        name='Add numbers',
+        domino_job_config=DominoJobConfig(Command="python simple_math_add.py"),
+        inputs={'first_value': int, 'second_value': int},
+        outputs={'sum': int},
+        use_latest=True
+    )
+    sum = add_task(first_value=a, second_value=b)
+
+    # Create second task
+    sqrt_task = DominoJobTask(
+        name='Square root',
+        domino_job_config=DominoJobConfig(Command="python simple_math_sqrt.py"),
+        inputs={'value': int},
+        outputs={'sqrt': float},
+        use_latest=True
+    )
+    sqrt = sqrt_task(value=sum)
+
+    return sqrt
